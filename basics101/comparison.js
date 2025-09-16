cb11127be9d165c00210f827b5bb53c7f6f64b14
avishka8 <avishka1108@gmail.com>
2025-09-16 22:16:08 +0530

    operations and comparisions

---
+++ basics101/comparison.js
@@ -0,0 +1,9 @@
+console.log(10>2);
+console.log(2<10);
+console.log(1==10);
+console.log(4!=10);
+console.log("2" > 1);
+console.log(null>0);
+console.log(null>=0);
+
+
--- basics101/conv-op.js
+++ basics101/conv-op.js
@@ -16,10 +16,27 @@ let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn)
 
-// 1=> true; 0=> false; avishka=>true
+// 1=> true
+// 0=> false
+// avishka=>true
 
 let someNumber = 33
 
 let StringNumber = String(someNumber)
 console.log(StringNumber)
-console.log(typeof StringNumber )
+console.log(typeof StringNumber)
+
+
+
+
+// ****** operations ****** 
+
+let value = 3
+let negVal = -value
+console.log(negVal)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+
+console.log(true);
