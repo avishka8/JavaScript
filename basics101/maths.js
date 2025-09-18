5371cc041880701d721e6b859ab82264cd196b91
avishka8 <avishka1108@gmail.com>
2025-09-18 13:19:23 +0530

    numbers and math in js

---
+++ basics101/maths.js
@@ -0,0 +1,23 @@
+const score = 400 
+//console.log(score);
+
+const balance = new Number (100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const num=121.38215
+//console.log(num.toPrecision(3));
+
+const num1 = 1000000000
+//console.log(num1.toLocaleString('en-IN'));
+
+
+//  MATHSSSSSSSSS //
+
+console.log(Math);
+console.log(Math.abs(-9));
+
+
+
