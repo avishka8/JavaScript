b622c3fe511e051d665c311ed09b5ae6eec5bdee
avishka8 <avishka1108@gmail.com>
2025-09-24 11:55:41 +0530

    while and do-while loops

---
+++ iterations/loop2.js
@@ -0,0 +1,22 @@
+let i = 0
+
+while (i<=12) {
+    //console.log(`value of i is ${i}`);
+    i = i+2;
+}
+
+let myarr = [ 'ironman', 'batman', 'thor']
+let arr = 0
+
+while(arr<myarr.length){
+    //console.log(myarr[arr]);
+    arr = arr+1;
+}
+
+
+let score = 11
+
+do{
+    console.log(`score is ${score}`);
+    score++    
+} while (score<10);
